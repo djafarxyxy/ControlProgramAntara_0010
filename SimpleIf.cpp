a97135dda5d9fcd9af03ea46e563e13c1c2cf908
djafarxyxy <farhanch12344@gmail.com>
2026-07-16 16:28:28 +0700

    Menambahkan logika if untuk mengoreksi nilai jika X bernilai 5 dan menampilkan output angka yang diberikan

--- SimpleIf.cpp
+++ SimpleIf.cpp
@@ -7,3 +7,10 @@ int main() {
     int X;
     srand(time(0));
     X = rand() % 10;
+    if (X == 5) {
+        X = X + 1;
+        cout << "nilai X sudah dikoreksi menjadi " << X << endl;
+    }
+    cout << "Bilangan yang diberikan = " << X << endl;
+    return 0;
+}
